b481466380f4ed31bea054117d48ce0e75aeb987
Ashutosh Sharma <minato@Ashutoshs-MacBook-Air.local>
2026-04-06 22:03:31 +0530

    reset folder

--- week1/website-summarizer/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from website-summarizer!")
-
-
-if __name__ == "__main__":
-    main()
--- week2/full-stack-kanban/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from full-stack-kanban!")
-
-
-if __name__ == "__main__":
-    main()
